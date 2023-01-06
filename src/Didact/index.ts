ff60f1dc66a7933fc98b0da4989ad8253001843f
aio <aioet@qq.com>
2023-01-06 14:51:30 +0800

    feat: 添加注释

--- src/Didact/index.ts
+++ src/Didact/index.ts
@@ -3,16 +3,37 @@
  */
 import { TEXT_ELEMENT } from "../OwnReact"
 
+/**
+ * 元素结构
+ */
 interface Element {
+  /**
+   * 元素类型
+   */
   type: typeof TEXT_ELEMENT | string
+  /**
+   * 元素属性
+   */
   props: {
     children: Element[]
   }
 }
-interface Instance {
-  dom: HTMLElement
-  element: Element
 
+/**
+ * 当前元素实例
+ */
+interface Instance {
+  /**
+   * 当前dom
+   */
+  dom: HTMLElement
+  /**
+   * 新的节点
+   */
+  element: Element
+  /**
+   * 子元素实例
+   */
   childInstances: Instance[]
 }
 
@@ -50,35 +71,46 @@ export class Didact {
    * @param element 新的节点
    */
   reconcile(parentDom: HTMLElement, instance: Instance, element: Element): null | Instance {
+    // 实例不存在 直接新建
     if (instance === null) {
       const newInstance = this.instantiate(element)
       parentDom.appendChild(newInstance.dom)
       return newInstance
     } else if (element === null) {
+      // 新的元素不存在直接删除
       parentDom.removeChild(instance.dom)
       return null
     } else if (instance.element.type === element.type) {
+      // 相同元素 更新属性
       this.updateDomProperties(instance.dom, instance.element.props, element.props)
+      //  递归处理子元素
       instance.childInstances = this.reconcileChildren(instance, element)
       instance.element = element
       return instance
     } else {
+      // 直接替换掉
       const newInstance = this.instantiate(element)
       parentDom.replaceChild(newInstance.dom, instance.dom)
       return newInstance
     }
   }
 
+  /**
+   * 对比子元素的实例
+   * 返回新的 元素实例
+   * @param instance 元素实例
+   * @param element 元素
+   */
   reconcileChildren(instance: Instance, element: Element): Instance[] {
     const dom = instance.dom
     const childInstance = instance.childInstances
     const children = element.props.children
-    const newChildInstances: any = []
+    const newChildInstances: Array<Instance> = []
 
     const len = Math.max(childInstance.length, children.length)
-
+    // 遍历比较实例和元素
     for (let i = 0; i < len; i++) {
-      newChildInstances.push(this.reconcile(dom, childInstance[i], children[i]))
+      newChildInstances.push(this.reconcile(dom, childInstance[i], children[i]) as Instance)
     }
     return newChildInstances.filter((instance) => instance !== null)
   }
